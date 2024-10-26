138b58489395a5c5f503ceaec397c1f7c2779d1e
Ben Healy <benjamin.p.healy@gmail.com>
2024-10-26 09:20:41 +0100

    Day 3 part 2

--- Day03/src/main/java/com/ben/aoc/App.java
+++ Day03/src/main/java/com/ben/aoc/App.java
@@ -7,6 +7,7 @@ public class App {
     public static void main( String[] args )
     {
         Day3 day3 = new Day3();
-        System.out.println("puzzle 1: " + day3.puzzle1(361527)); //102 too low
+        System.out.println("puzzle 1: " + day3.puzzle1(361527));
+        System.out.println("puzzle 2: " + day3.puzzle2(361527));
     }
 }
--- Day03/src/main/java/com/ben/aoc/Day3.java
+++ Day03/src/main/java/com/ben/aoc/Day3.java
@@ -1,11 +1,22 @@
 package com.ben.aoc;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Day3 {
+
+    private final Map<Point<Integer>, Integer> datastore = new HashMap<>();
 
     public long puzzle1(int input) {
         CartesianIntPoint location = getLocation(input);
 
         return location.manhattanDistance(new CartesianIntPoint(0,0));
+    }
+
+    public long puzzle2(int input) {
+
+        return getValueAbove(input);
     }
 
     private CartesianIntPoint getLocation(int input){
@@ -32,5 +43,41 @@ public class Day3 {
 
         }
         return location;
+    }
+
+    private int getValueAbove(int input){
+        CartesianIntPoint location = new CartesianIntPoint(0,0);
+        datastore.put(location, 1);
+        int movesBeforeTurn = 1;
+        int turnCount = 0;
+        int i = 1;
+        int moves = 0;
+        Direction dir = new Direction(Direction.Dir.RIGHT);
+        while (i < input){
+            while(moves < movesBeforeTurn && i < input){
+                location = (CartesianIntPoint) location.getByDirection(dir);
+                i = getValue(location);
+                datastore.put(location, i);
+                moves ++;
+            }
+            dir = dir.rotateAntiClockwise();
+            turnCount++;
+            if(turnCount == 2){
+                movesBeforeTurn++;
+                turnCount = 0;
+            }
+            moves = 0;
+
+        }
+        return i;
+    }
+
+    private int getValue(CartesianIntPoint location){
+        List<Point<Integer>> neighbours = location.allNeighbours(true);
+        int result = 0;
+        for(Point<Integer> coord : neighbours){
+            result+=datastore.getOrDefault(coord, 0);
+        }
+        return result;
     }
 }
