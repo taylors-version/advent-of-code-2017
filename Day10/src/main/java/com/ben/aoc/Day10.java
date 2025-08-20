38a89af346802e4adabc532a09bd71bc594c2c1d
Ben Healy <139165148+taylors-version@users.noreply.github.com>
2025-08-20 15:09:46 +0100

    Day 10 part 2

--- Day10/src/main/java/com/ben/aoc/Day10.java
+++ Day10/src/main/java/com/ben/aoc/Day10.java
@@ -2,6 +2,8 @@ package com.ben.aoc;
 
 import com.ben.aoc.collection.CircularList;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Day10 {
@@ -26,8 +28,34 @@ public class Day10 {
         return circle.get(0) * circle.get(1);
     }
 
-    public long puzzle2(String input) {
-        return 0;
+    public String puzzle2(String input) {
+        List<Integer> lengths = getAsciiLengths(input);
+        CircularList<Integer> circle = createCircle();
+        int start = 0;
+        int skip = 0;
+        for(int round = 0; round < 64; round ++){
+            for(Integer length : lengths) {
+
+                List<Integer> sublist = circle.subList(start, start + length);
+
+                for (int pos = 0; pos < length; pos++) {
+                    circle.set(start + pos, sublist.get((length - 1) - pos));
+                }
+                start += length + skip;
+                skip++;
+            }
+        }
+        List<Integer>sparseHash = new ArrayList<>();
+        for(int i = 0; i < 16; i++){
+            List<Integer> sublist = circle.subList(i*16, (i+1) * 16);
+            sparseHash.add(sparse(sublist));
+        }
+
+        StringBuilder result = new StringBuilder();
+        for(Integer hex : sparseHash){
+            result.append(String.format("%02x", hex));
+        }
+        return result.toString();
     }
 
     private CircularList<Integer> createCircle(){
@@ -38,6 +66,23 @@ public class Day10 {
         }
 
         return circle;
+    }
+
+    private List<Integer> getAsciiLengths(String input){
+        List<Integer> lengths = new ArrayList<>();
+        for(char c : input.toCharArray()){
+            lengths.add((int) c);
+        }
+        lengths.addAll(Arrays.asList(17, 31, 73, 47, 23));
+        return lengths;
+    }
+
+    public int sparse(List<Integer> input){
+        int result = input.get(0);
+        for (int i = 1; i< input.size(); i++){
+            result = result ^ input.get(i);
+        }
+        return result;
     }
 
 }
--- Day10/src/test/java/com/ben/aoc/AppTest.java
+++ Day10/src/test/java/com/ben/aoc/AppTest.java
@@ -2,6 +2,8 @@ package com.ben.aoc;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
 
 public class AppTest {
@@ -11,6 +13,12 @@ public class AppTest {
     public void TestPuzzle1(){
         assertEquals("should be 1,0,...",0, day10.puzzle1("2"));
         assertEquals("should be 2,1,0,...",2, day10.puzzle1("3"));
+    }
+
+    @Test
+    public void TestPuzzle2SampleInputs(){
+        assertEquals("a2582a3a0e66e6e86e3812dcb672a272", day10.puzzle2(""));
+        assertEquals("3efbe78a8d82f29979031a4aa0b16a9d", day10.puzzle2("1,2,3"));
     }
 
 }
