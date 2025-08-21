f05e24a47e14213b744fd43089ae6677ab316a6c
Ben Healy <139165148+taylors-version@users.noreply.github.com>
2025-08-21 15:07:17 +0100

    Day 11 part 2

--- Day11/src/main/java/com/ben/aoc/App.java
+++ Day11/src/main/java/com/ben/aoc/App.java
@@ -8,7 +8,7 @@ public class App {
     {
         Day11 day11 = new Day11();
         List<String> lines = Util.readFile(App.class, "input.txt");
-        System.out.println("puzzle 1: " + day11.puzzle1(lines.get(0)));//808 (after normalising, ne484 + n324)
+        System.out.println("puzzle 1: " + day11.puzzle1(lines.get(0)));
         System.out.println("puzzle 2: " + day11.puzzle2(lines.get(0)));
     }
 }
--- Day11/src/main/java/com/ben/aoc/Day11.java
+++ Day11/src/main/java/com/ben/aoc/Day11.java
@@ -4,23 +4,37 @@ package com.ben.aoc;
 import org.javatuples.Pair;
 
 import java.util.*;
-import java.util.stream.Stream;
 
 public class Day11 {
 
     public long puzzle1(String input) {
-        List<String> directions = Arrays.asList(input.split(","));
-
-        return bfs(getLocation(directions));
-
+        return (getDistance(Arrays.asList(input.split(","))));
     }
 
     public long puzzle2(String input) {
-        return 0;
+        long max = 0;
+        List<String> directions = Arrays.asList(input.split(","));
+        for(int i = 1; i <= directions.size(); i++){
+            long length = getDistance(directions.subList(0, i));
+            max = Math.max(max, length);
+        }
+
+
+        return max;
     }
 
-    private Pair<Integer, Double> getLocation(List<String> directions){
-        double y = 0;
+    private long getDistance(List<String> directions){
+        Pair<Integer, Integer> location = getLocation(directions);
+        int x = location.getValue0();
+        int y = location.getValue1();
+        if(y <= x){
+            return x;
+        }
+        return x + (y-x)/2;
+    }
+
+    private Pair<Integer, Integer> getLocation(List<String> directions){
+        int y = 0;
         int x = 0;
         int n = Collections.frequency(directions, "n");
         int ne = Collections.frequency(directions, "ne");
@@ -34,58 +48,9 @@ public class Day11 {
         nw = nw-se;
 
         x = ne-nw;
-        y=n+0.5*(ne+nw);
+        y=2*n+(ne+nw);
 
         return new Pair<>(Math.abs(x),Math.abs(y));
     }
-
-    private List<String> directionsFromMap(Map<String, Integer> map){
-        List<String> directions = new ArrayList<>();
-        for(int i = 0; i < map.getOrDefault("n", 0); i++){
-            directions.add("n");
-        }
-        for(int i = 0; i < map.getOrDefault("ne", 0); i++){
-            directions.add("ne");
-        }
-
-        return directions;
-    }
-
-    private int bfs(Pair<Integer, Double> location){
-        int steps = 0;
-
-        HashMap<String, Integer> moves = new HashMap<>();
-        moves.put("n", 0);
-        moves.put("ne", 0);
-
-        Queue<HashMap<String, Integer>> queue = new ArrayDeque<>();
-        queue.add(moves);
-        Set<HashMap<String, Integer>> visited = new HashSet<>();
-
-        HashMap<String, Integer> currentNode;
-
-        while(!queue.isEmpty()){
-            currentNode = queue.remove();
-            int n = currentNode.getOrDefault("n", 0);
-            int ne = currentNode.getOrDefault("ne", 0);
-            if(getLocation(directionsFromMap(currentNode)).equals(location)){
-                return n+ne;
-            }else{
-                visited.add(currentNode);
-
-                HashMap<String, Integer> nMap = new HashMap<>();
-                HashMap<String, Integer> neMap = new HashMap<>();
-                nMap.put("n", n+1);
-                nMap.put("ne", ne);
-                neMap.put("n", n);
-                neMap.put("ne", ne+1);
-                queue.add(nMap);
-                queue.add(neMap);
-            }
-        }
-
-        return steps;
-    }
-
 
 }
