e52cdda5067fa46957a2447fa0dbf24476719f28
Ben Healy <139165148+taylors-version@users.noreply.github.com>
2025-09-01 08:11:35 +0100

    Day 21 part 2

--- Day21/src/main/java/com/ben/aoc/Day21.java
+++ Day21/src/main/java/com/ben/aoc/Day21.java
@@ -13,24 +13,38 @@ public class Day21 {
             enhancements.add(new Picture(split[0], new Picture(split[1])));
         }
         for(int i = 0; i < iterations; i++){
-            List<Picture> split = artwork.split();
-            List<Picture> translated = new ArrayList<>();
-            for(Picture p : split){
-                for(Picture e : enhancements){
-                    if(e.matches(p)){
-                        translated.add(e.output);
-                        break;
-                    }
-                }
-            }
-            artwork = new Picture(translated);
+
+            artwork = iterate(artwork, enhancements);
         }
 
         return artwork.numberOfOn();
     }
 
     public long puzzle2(List<String> input) {
-        return 0;
+        Picture artwork = new Picture(".#./..#/###");
+        List<Picture> enhancements = new ArrayList<>();
+        for(String line : input){
+            String[] split = line.split(" => ");
+            enhancements.add(new Picture(split[0], new Picture(split[1])));
+        }
+        for(int i = 0; i < 18; i++){
+            artwork = iterate(artwork, enhancements);
+        }
+        return artwork.numberOfOn();
+    }
+
+    private Picture iterate(Picture artwork, List<Picture> enhancements){
+        List<Picture> split = artwork.split();
+        List<Picture> translated = new ArrayList<>();
+        for(Picture p : split){
+            for(Picture e : enhancements){
+                if(e.matches(p)){
+                    translated.add(e.output);
+                    break;
+                }
+            }
+        }
+        return new Picture(translated);
     }
 
 }
